ae035ed67531bfae671459ad189e052735b16546
Pedro Paulo Gonçalves <pedropaulogoncalves58@gmail.com>
2025-09-16 09:25:18 -0300

    Add files via upload

---
+++ aula-06/if.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>if somente - javascript</title>
+    <style>
+        #resposta {
+            font: solid 16px
+        }
+    </style>
+</head>
+<body>
+    <h1>if somente - PHP</h1>
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">;
+         <label for="idade">idade:</label>
+         <input type="text" id="idade" name="idade"/>
+         <input type="submit" value="verificar"/>
+      </form>
+    <hr>
+    <?php
+    //if (isset($_POST))
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $_idade = $_POST["idade"];
+            $_resultado = "voce e menor de idade!";
+
+            if ($_idade >= 18) {
+                $_resultado = "voce e maior  de idade!";
+            }   
+            echo "<p id='resposta'>$_resultado</p>";
+        }
+ ?>
+</body>
+</html>
---
+++ aula-06/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laços de repetição</title>
+</head>
+<body>
+    <h1>Laço if</h1>
+    <a href="if.html">iF somente (javascript)</a><br>
+    <a href="if.html">iF somente (PHP)</a>
+    <h1>Laço if lese</h1>
+    <a href="if.else.html">iF else html (javascript)</a><br>
+    <a href="if.else.php">iF else PHP(PHP)</a>
+    <h1>Laço if lese</h1>
+    <a href="if.elseif.html">iF else html (javascript)</a><br>
+    <a href="if.elseif.php">iF else PHP(PHP)</a>
+</body>
+</html>
